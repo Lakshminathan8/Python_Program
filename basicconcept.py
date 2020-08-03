1a95bef599736543f3f0eb20cadf207df0468986
Nathan <67477170+Lakshminathan8@users.noreply.github.com>
2020-08-03 23:17:17 +0530

    Basic python program

---
+++ basicconcept.py
@@ -0,0 +1,34 @@
+import string
+import math
+
+#Single ling comments
+print("Welcome to Python")
+
+'''This is multi
+line commment'''
+
+print("""Multi
+line 
+comment print""")
+
+a = 5
+print(type(a))
+
+b = 20
+print(type(b))
+
+
+a = 20
+c = a+b
+
+d = "Addition:"
+print(d,c)
+
+e=100+3j
+print("print e value",e,"is",type(e))
+
+f = complex(2,4)
+print(type(f))
+print(f)
+
+
