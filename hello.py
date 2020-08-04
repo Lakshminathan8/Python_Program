7654863a645851be7a7cd2ceea19466be634b80a
Lakshminathan P <LAKSHMINATHAN@Lakshminathans-MacBook-Pro.local>
2020-08-04 23:48:56 +0530

    function programming

--- hello.py
+++ hello.py
@@ -1,1 +1,41 @@
-print("hello world")
+def greet_user():
+	"""Display a simple greeting"""
+	print("hello world")
+
+greet_user()
+
+def greet_user(username):
+	print("Hello, "+ username + "!")
+
+greet_user('jesse')
+greet_user('diana')
+
+def describe_pet(animal, name):
+	print("\n I have a "+ animal)
+	print("Its name is "+name)
+
+describe_pet("dog", 'vicky')
+
+def desc_pet(animal, name):
+        print("\n I have a "+ animal+".")
+        print("Its name is "+name+".")
+
+desc_pet(animal="cow",name="vicky")
+
+def descr_pet(name,animal='Tiger'):
+        print("\n I have a "+ animal+".")
+        print("Its name is "+name+".")
+
+descr_pet(animal="cow",name="ganesh")
+descr_pet('dog','hamster')
+descr_pet('gihub')
+
+def des_pet(animal, name=None):
+        print("\n I have a "+ animal+".")
+        if name: 
+           print("Its name is " + name + ".")
+
+des_pet('hamster','harry')
+des_pet('snake')
+
+"""def get_full_name"""
